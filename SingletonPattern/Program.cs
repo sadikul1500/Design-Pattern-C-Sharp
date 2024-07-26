930dbc2f87f92b18390bc40f1cc5bc639b75e89d
Sadikul Haque Sadi <bsse1003@iit.du.ac.bd>
2024-07-26 18:25:14 +0600

    singleton pattern

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SingletonPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+            var s = Singleton.GetInstance;
+            s.PrintDetails("Hello from Sadi");
+
+            var r = Singleton.GetInstance;
+            r.PrintDetails("Hello from Riyad");
+
+            //var nested = new Singleton.NestedSingleton();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,33 @@
+﻿namespace SingletonPattern;
+public sealed class Singleton
+{
+    private static int InstanceCounter = 0;
+    public static Singleton Instance = null;
+    public static Singleton GetInstance
+    {
+       get
+       {
+            if(Instance == null)
+            {
+                Instance = new Singleton();
+            }
+            return Instance;
+       } 
+    }
+
+    private Singleton()
+    {
+        InstanceCounter++;
+        Console.WriteLine("Counter Value " + InstanceCounter);
+    }
+
+    public void PrintDetails(string message)
+    { 
+        Console.WriteLine(message); 
+    }
+
+    //public class NestedSingleton : Singleton
+    //{
+
+    //}
+}
